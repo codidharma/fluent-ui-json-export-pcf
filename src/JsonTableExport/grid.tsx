8bb926f6ac374dde8017753f6424e98f7f80e0c6
codidharma <codidharma@gmail.com>
2022-11-25 20:31:38 +0530

    Added dialog footer

--- src/JsonTableExport/grid.tsx
+++ src/JsonTableExport/grid.tsx
@@ -1,7 +1,8 @@
 import { ComponentProperties } from './types/componentProps';
 import * as React from 'react';
 import { CommandBar, ICommandBarItemProps } from '@fluentui/react/lib/CommandBar';
-
+import { Dialog, DialogFooter, DialogType, IDialogContentProps } from '@fluentui/react/lib/Dialog';
+import { PrimaryButton, DefaultButton } from '@fluentui/react/lib/Button';
 
 
 export const ExportComponent = React.memo((props: ComponentProperties) => {
@@ -10,6 +11,22 @@ export const ExportComponent = React.memo((props: ComponentProperties) => {
     } = props;
 
     const [hideDialogFlag, setHideDialogFlag] = React.useState(true);
+
+    const dialogStyles = {main:{maxWidth: 200}};
+
+    const modalProps = {
+        isBlocking: false,
+        topOffsetFixed: true,
+        styles: dialogStyles,
+    };
+
+    const dialogContentProps : IDialogContentProps = {
+        type: DialogType.normal,
+        title: 'Download option selection form',
+        subText: 'Please select proper configuration for the data download option.',
+        showCloseButton: false
+
+    };
     
     const commandbarProps: ICommandBarItemProps[] = [
         {
@@ -26,7 +43,17 @@ export const ExportComponent = React.memo((props: ComponentProperties) => {
         <>
             <CommandBar items={commandbarProps}>               
             </CommandBar>
-            
+            <Dialog
+                hidden = {hideDialogFlag}
+                onDismiss = {() => setHideDialogFlag(!hideDialogFlag)}
+                dialogContentProps = {dialogContentProps}
+                modalProps = {modalProps}
+            >
+                <DialogFooter>
+                    <PrimaryButton onClick={() => {}} text="Confirm"/>
+                    <DefaultButton onClick={() => {}} text="Cancel" />
+                </DialogFooter> 
+            </Dialog>            
         </>
     );
 
