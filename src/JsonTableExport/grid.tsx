556ad23834b86b02dc6dc5305e0049145d079b51
codidharma <codidharma@gmail.com>
2022-11-25 23:54:42 +0530

    Added Logic to export as Excel

--- src/JsonTableExport/grid.tsx
+++ src/JsonTableExport/grid.tsx
@@ -1,9 +1,14 @@
-import { ComponentProperties } from './types/componentProps';
 import * as React from 'react';
+import { ComponentProperties } from './types/componentProps';
 import { CommandBar, ICommandBarItemProps } from '@fluentui/react/lib/CommandBar';
 import { Dialog, DialogFooter, DialogType, IDialogContentProps } from '@fluentui/react/lib/Dialog';
 import { PrimaryButton, DefaultButton } from '@fluentui/react/lib/Button';
-
+import { Stack } from '@fluentui/react/lib/Stack';
+import { IDropdownOption, Dropdown, IDropdownProps } from '@fluentui/react/lib/Dropdown';
+import { ExportType } from './types/exportTypes';
+import { Icon} from '@fluentui/react/lib/Icon';
+import { TextField } from '@fluentui/react/lib/TextField';
+import { ExportAsXlsx } from './utils/exportAsXlsx';
 
 export const ExportComponent = React.memo((props: ComponentProperties) => {
     const {
@@ -11,6 +16,8 @@ export const ExportComponent = React.memo((props: ComponentProperties) => {
     } = props;
 
     const [hideDialogFlag, setHideDialogFlag] = React.useState(true);
+    const [selectedExportOption, setSelectedExportOption] = React.useState('');
+    const [exportFileName, setExportFileName] = React.useState('');
 
     const dialogStyles = {main:{maxWidth: 200}};
 
@@ -22,9 +29,84 @@ export const ExportComponent = React.memo((props: ComponentProperties) => {
 
     const dialogContentProps : IDialogContentProps = {
         type: DialogType.normal,
-        title: 'Download option selection form',
-        subText: 'Please select proper configuration for the data download option.',
+        title: 'Export option selection form',
+        subText: 'Please select proper configuration for the data export',
         showCloseButton: false
+
+    };
+
+    const exportTypesOptions: IDropdownOption[] = [
+        {
+            key : ExportType.XLSX,
+            text: 'Export as Excel',
+            data: { icon: 'ExcelDocument'}
+        },
+        {
+            key : ExportType.CSV,
+            text: 'Export as CSV',
+            data: { icon: 'ExcelDocument'}
+        }
+    ] as IDropdownOption[];
+
+    const iconStyles = { marginRight: '8px' };
+
+    //const dropdownStyles = {dropdown: {width: 300}};
+
+    const onRenderOption = (option?: IDropdownOption) : JSX.Element => {
+        return(
+            <div>
+                {option!.data && option!.data.icon && (
+                    <Icon style={iconStyles} iconName={option!.data.icon} aria-hidden = 'true' title = {option!.data.icon}/>
+                )}
+                <span>{option!.text}</span>
+            </div>
+        );
+    };
+
+    const onExportOptionDropDownChange = (event: React.FormEvent<HTMLDivElement>, option?: IDropdownOption, index?: number): void => {
+        setSelectedExportOption(option!.key.toString());
+        console.log(option!.key.toString());
+
+    };
+
+    const onFileNameChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string): void => {
+        setExportFileName(newValue!);
+        console.log(newValue!);
+    };
+
+    const onCancelExportDialog = () => {
+        setHideDialogFlag(true);
+        setSelectedExportOption('');
+        setExportFileName('');
+    };
+
+    const formatExportData = (): any[] => {
+        const items = JSON.parse(data) as any[];
+        const columns = Object.keys(items[0]);
+        const exportableData : any[] = [];
+        let exportableObject : any = {};
+        items.forEach((item) => {
+            columns.forEach((column) => {
+                exportableObject[column] = item[column];
+            });
+            exportableData.push(exportableObject);
+            exportableObject = {};
+        });
+
+        return exportableData;
+    };
+
+
+    const onConfirmExportDialog = () => {
+        setHideDialogFlag(true);
+        const exportableData = formatExportData();
+        switch(selectedExportOption as ExportType)
+        {
+        case ExportType.XLSX:
+            ExportAsXlsx(exportableData, exportFileName + '.xlsx');
+            break;
+        }
+        
 
     };
     
@@ -49,9 +131,27 @@ export const ExportComponent = React.memo((props: ComponentProperties) => {
                 dialogContentProps = {dialogContentProps}
                 modalProps = {modalProps}
             >
+                <Stack>
+                    <Stack.Item>
+                        <Dropdown
+                            options={exportTypesOptions}
+                            placeholder = 'Select export option'
+                            label='Export Option'
+                            onRenderOption={onRenderOption}
+                            onChange= {onExportOptionDropDownChange}
+                        />   
+                    </Stack.Item>
+                    <Stack.Item>
+                        <TextField
+                            placeholder='Enter the export file name'
+                            label='Export File Name'
+                            onChange={onFileNameChange}
+                        />
+                    </Stack.Item>
+                </Stack>
                 <DialogFooter>
-                    <PrimaryButton onClick={() => {}} text="Confirm"/>
-                    <DefaultButton onClick={() => {}} text="Cancel" />
+                    <PrimaryButton onClick={onConfirmExportDialog} text = 'Confirm' />
+                    <DefaultButton onClick={onCancelExportDialog} text = 'Cancel' />
                 </DialogFooter> 
             </Dialog>            
         </>
--- src/JsonTableExport/types/exportTypes.tsx
+++ src/JsonTableExport/types/exportTypes.tsx
@@ -1,4 +1,4 @@
 export enum ExportType{
-    XLSX,
-    CSV
+    XLSX = 'xlsx',
+    CSV = 'csv'
 }
