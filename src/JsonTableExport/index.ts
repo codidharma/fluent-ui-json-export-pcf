ef0e85be61033a762d16781c48f7d268a4c6712b
codidharma <codidharma@gmail.com>
2022-12-04 10:22:48 +0530

    Changed the control to Virtual

--- src/JsonTableExport/index.ts
+++ src/JsonTableExport/index.ts
@@ -3,12 +3,11 @@ import * as ReactDOM from 'react-dom/client';
 import {IInputs, IOutputs} from './generated/ManifestTypes';
 import { ExportComponent } from './grid';
 
-export class JsonTableExport implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+export class JsonTableExport implements ComponentFramework.ReactControl<IInputs, IOutputs> {
 
     jsonData: string|null = null;
     container: HTMLDivElement;
-    root: ReactDOM.Root;
-    
+
     constructor()
     {
         //do nothing
@@ -21,19 +20,24 @@ export class JsonTableExport implements ComponentFramework.StandardControl<IInpu
         
     }
 
-    public updateView(context: ComponentFramework.Context<IInputs>): void
+    public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement
     {
         // Add code to update control view
 
         this.jsonData = context.parameters.Data.raw;
 
-        this.root = ReactDOM.createRoot(this.container);
-        this.root.render(
-            React.createElement(ExportComponent, {
-                data: this.jsonData!
+        return React.createElement(ExportComponent, {
+            data: this.jsonData!
+            
+        });
+
+        // this.root = ReactDOM.createRoot(this.container);
+        // this.root.render(
+        //     React.createElement(ExportComponent, {
+        //         data: this.jsonData!
                 
-            })
-        );
+        //     })
+        // );
 
         // ReactDOM.render(
         //     React.createElement(ExportComponent, {
@@ -51,7 +55,7 @@ export class JsonTableExport implements ComponentFramework.StandardControl<IInpu
 
     public destroy(): void
     {
-        this.root.unmount
+        //this.root.unmount
         //ReactDOM .unmountComponentAtNode(this.container);
         
     }
